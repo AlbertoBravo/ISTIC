415a7e0e019e02f92df6622671a5210e31610f25
Alberto Bravo <albertobravog4@gmail.com>
2018-11-26 01:40:30 +0100

    Include all again jax commit

---
+++ src/main/java/io/swagger/api/Bootstrap.java
@@ -0,0 +1,31 @@
+package io.swagger.api;
+
+import io.swagger.jaxrs.config.SwaggerContextService;
+import io.swagger.models.*;
+
+import io.swagger.models.auth.*;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+
+public class Bootstrap extends HttpServlet {
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    Info info = new Info()
+      .title("Swagger Server")
+      .description("API que se encargue del tratamiento gráfico de los datos procesados en función de lo que solicite el usuario gráficas, valoración, etc.")
+      .termsOfService("")
+      .contact(new Contact()
+        .email("albravog@alumnos.unex.es"))
+      .license(new License()
+        .name("")
+        .url("http://unlicense.org"));
+
+    ServletContext context = config.getServletContext();
+    Swagger swagger = new Swagger().info(info);
+
+    new SwaggerContextService().withServletConfig(config).updateSwagger(swagger);
+  }
+}
